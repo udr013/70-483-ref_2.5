db78fff6e2e5f5dcfd36c002f19e0623080a8c7a
Mark.Sukel <mark.sukel@allego.eu>
2019-05-23 01:40:39 +0200

    added examples

---
+++ CodeDomObject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+
+namespace CodeDomObject
+{
+    class Program
+    {
+        //Code Document Object Model
+        static void Main(string[] args)
+        {
+            CodeCompileUnit codeCompileUnit = new CodeCompileUnit();
+
+            //create a namespace to hold the types we are going to create
+            CodeNamespace personnalNamespace = new CodeNamespace("Personnel");
+            //import the system namespace
+            personnalNamespace.Imports.Add(new CodeNamespaceImport("System"));
+            
+            //create a Person class
+            CodeTypeDeclaration personClass = new CodeTypeDeclaration("Person");
+            personClass.IsClass = true;
+            personClass.TypeAttributes = System.Reflection.TypeAttributes.Public;
+            
+            //Create a field to hold the name of person
+            CodeMemberField nameField = new CodeMemberField("String", "name");
+            nameField.Attributes = MemberAttributes.Private;
+            personClass.Members.Add(nameField);
+
+            //Add the person class to personnelNamespace and addit to compileunit
+            personnalNamespace.Types.Add(personClass);
+            codeCompileUnit.Namespaces.Add(personnalNamespace);
+
+            //Create a provider to parse the document and  a space to send output
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            StringWriter s = new StringWriter();
+            //Set options for parsing, we use the defaults...
+            CodeGeneratorOptions options = new CodeGeneratorOptions();
+
+            //Generate the C# source from the codeDOM
+            provider.GenerateCodeFromCompileUnit(codeCompileUnit, s, options);
+            //Close output stream
+            s.Close();
+
+            Console.WriteLine(s.ToString());
+         
+        }
+    }
+}
---
+++ LambdaExpressionTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace LambdaExpressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //buid the expression tree:
+            //Expression<Func<int, int>> square = num => num * num;
+
+            //the parameter for the expression is an intager
+            ParameterExpression numParam = Expression.Parameter(typeof(int), "num");
+
+            //the operation to be preformed is to square the parameter
+            BinaryExpression squareOperation = Expression.Multiply(numParam, numParam);
+
+            //This creates an expression tree that describes the square operation
+            Expression<Func<int, int>> square = Expression.Lambda<Func<int, int>>(squareOperation, new ParameterExpression[] { numParam });
+
+            //Compile tje tree to make an executable method and assign itto a delegate
+            Func<int, int> doSquare = square.Compile();
+
+            //Call the delegate
+            Console.WriteLine("Square of 2: {0}", doSquare(2));
+        }
+    }
+}
