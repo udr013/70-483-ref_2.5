2d4774737d7703ce3dcf39deb4db402fd1a92ec5
Mark.Sukel <mark.sukel@allego.eu>
2019-10-18 00:09:54 +0200

    added assemblyExmple

---
+++ AssemblyObject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyObject
+{
+    class Program
+    { 
+        class MyModule
+    {
+            int bla = 7;
+    }
+
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Console.WriteLine("Full name: {0}", assembly.FullName);
+            AssemblyName name = assembly.GetName();
+            Console.WriteLine("Major Version: {0}", name.Version.Major);
+            Console.WriteLine("Minor Version: {0}", name.Version.Minor);
+            Console.WriteLine("In global assembly cache: {0}", assembly.GlobalAssemblyCache);
+            foreach (Module assemblyModule in assembly.Modules)
+            {
+                Console.WriteLine("module: {0}", assemblyModule.Name);
+                foreach (Type moduleType in assemblyModule.GetTypes())
+                {
+                    Console.WriteLine("    modelType:{0}", moduleType.Name);
+                    foreach (MemberInfo memberinfo in moduleType.GetMembers())
+                    {
+                        Console.WriteLine("       memberinfo {0}", memberinfo.Name);
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LambdaExpressionTree/MultiplyToAdd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace LambdaExpressionTree
+{
+    class MultiplyToAdd : ExpressionVisitor
+    {
+        public Expression Modify(Expression expression)
+        {
+            return Visit(expression);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression b)
+        {
+            if(b.NodeType == ExpressionType.Multiply)
+            {
+                Expression left = this.Visit(b.Left);
+                Expression right = this.Visit(b.Right);
+
+                //Make this binary expression an Add rather than a multiply operation.
+                return Expression.Add(left, right);
+            }
+
+            return base.VisitBinary(b);
+        }
+    }
+}
